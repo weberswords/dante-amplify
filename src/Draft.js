3a842fbb634c7f94dedbf7253da7017c9b38c51c
Webs <weberswords@gmail.com>
2021-08-13 15:39:20 -0700

    Add new artifact with wysiwyg

--- src/Draft.js
+++ src/Draft.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import MUIEditor, { MUIEditorState } from "react-mui-draft-wysiwyg";
+import { convertToRaw } from 'draft-js';
+import { createArtifact } from './graphql/mutations';
+import { graphqlOperation } from '@aws-amplify/api-graphql';
+import { API } from '@aws-amplify/api'
+import uuid from 'react-uuid';
 
 function Draft() { 
     const useStyles = makeStyles((theme) => ({
@@ -15,21 +21,56 @@ function Draft() {
     const [editorState, setEditorState] = React.useState(
       MUIEditorState.createEmpty(),
     )
+
+    const [titleState, setTitleState] = React.useState('');
+    
+    const onTitleChange = (event) => {
+      setTitleState(event.target.value)
+    }
+
     const onChange = newState => {
       setEditorState(newState)
     }
+    
+    const save = () => {
+      const rawContent = convertToRaw(editorState.getCurrentContent())
+      const newArtifact = {
+        content: rawContent,
+        id: uuid(),
+        portfolioID: 'gr4334t4tog345ht35',
+        title: titleState
+      }
+      try {
+        API.graphql(graphqlOperation(createArtifact, {input: newArtifact}));
+      } catch (error) {
+        console.log('error creating artifacts', error);
+      }
+      console.log(rawContent)
+    }
+  
     const classes = useStyles();
 
     return (
         
         <div className={classes.parentDiv}>
 
-            <h2>Create New Portfolio Piece</h2>
+            <h2>Create New Portfolio Artifact</h2>
+            <TextField
+              required
+              id="title"
+              label="Required"
+              defaultValue="Artifact Title"
+              variant="outlined"
+              onChange={onTitleChange}
+            />
             <MUIEditor editorState={editorState} onChange={onChange} />
+            
             <Button
              variant="contained"
              color="primary"
+             onClick={save}
             >Add Artifact</Button>
+            
         </div>
     )
   }
