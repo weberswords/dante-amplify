ff366931d9e99e757da9742b5cb5299cb80226a5
Webs <weberswords@gmail.com>
2021-08-13 10:57:41 -0700

    get portfolio artifacts

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { listArtifacts } from './graphql/queries';
+import { graphqlOperation } from '@aws-amplify/api-graphql';
+import { API } from '@aws-amplify/api'
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import CloseIcon from '@material-ui/icons/Close';
 import Draft from './Draft';
-import { FormatColorResetOutlined } from '@material-ui/icons';
 
 function Portfolio() { 
     const useStyles = makeStyles((theme) => ({
@@ -20,7 +22,23 @@ function Portfolio() {
     const handleClose = () => {
         setShowDraft(0);
       }
-  
+
+    const [artifacts, setArtifacts] = useState([]);
+    const fetchArtifacts = async () => {
+      try {
+        const artifactData = await API.graphql(graphqlOperation(listArtifacts));
+        const artifactList = artifactData.data.listArtifacts.items;
+        console.log('artifact list', artifactList);
+        setArtifacts(artifactList);
+      } catch (error) {
+        console.log('error fetching artifacts'. error);
+      }
+    };
+
+    useEffect(() => {
+      fetchArtifacts();
+    }, []);
+
     return (
         
         <div className={classes.parentDiv}>
